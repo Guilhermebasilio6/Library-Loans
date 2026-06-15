8d9402b796f9b1db10f8365da8a73397d8be9e4b
Aluno Lauro de Freitas <aluno.lauro@senaibahia.com.br>
2026-06-15 10:42:49 -0300

    correcao do model de membro

--- backend/src/models/Membro.js
+++ backend/src/models/Membro.js
@@ -1,34 +1,32 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const Membro = sequelize.define;
-("Membro",
-  {
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    cpf: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    telefone: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    dataCadastro: {
-      type: DataTypes.DATEONLY,
-      defaultValue: DataTypes.NOW,
-    },
-    status: {
-      type: DataTypes.ENUM("ativo", "suspenso", "inativo"),
-      defaultValue: "ativo",
-    },
-  });
+const Membro = sequelize.define("Membro", {
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  telefone: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  data_cadastro: {
+    type: DataTypes.DATEONLY,
+    defaultValue: DataTypes.NOW,
+  },
+  status: {
+    type: DataTypes.ENUM("ativo", "suspenso", "inativo"),
+    defaultValue: "ativo",
+  },
+});
 
 module.exports = Membro;
